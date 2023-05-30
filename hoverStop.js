84588538bc78568002e60f0fd436fe6a5171aa55
kyungmin <dakjijisalad@gmail.com>
2023-05-30 12:10:28 +0900

    img changed, animation added

--- hoverStop.js
+++
@@ -1,9 +0,0 @@
-const container = document.querySelector('.container');
-
-container.addEventListener('mousewheel', (e) => {
-  // Normalize the wheel delta across browsers
-  const normalizedWheelDelta = e.wheelDelta || -e.detail;
-
-  // Adjust scroll position based on wheel delta
-  container.scrollLeft += normalizedWheelDelta;
-});
